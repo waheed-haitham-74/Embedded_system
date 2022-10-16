95c6eec4457823ae10900de787f01c7876d66182
waheed haitham <115638466+waheed-haitham-74@users.noreply.github.com>
2022-10-16 23:00:20 +0200

    Assignments

---
+++ Assignments/C assignments/unit 2 lesson 3/HW_1/ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+    printf("enter an integer :");
+    fflush(stdin);fflush(stdout);
+    scanf("%d",&number);
+    printf("the number is :%d",number);
+}
