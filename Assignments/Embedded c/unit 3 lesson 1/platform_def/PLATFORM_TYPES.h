8d4b2bd38351ad3c88c63fd98849399c4730dd4d
waheed-haitham <66116052+waheed-haitham@users.noreply.github.com>
2022-11-19 16:35:19 +0200

    embedded c

---
+++ Assignments/Embedded c/unit 3 lesson 1/platform_def/PLATFORM_TYPES.h
@@ -0,0 +1,45 @@
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef signed char	        sint8;
+typedef unsigned char	        uint8;
+typedef signed short	        sint16;
+typedef unsigned short	        uint16;
+typedef signed int	        sint32;
+typedef unsigned int    	uint32;
+typedef signed long long        sint64;
+typedef unsigned long long	uint64;
+
+
+
+typedef volatile signed char		vint8_t;
+typedef volatile unsigned char		vuint8_t;
+typedef volatile signed short		vint16_t;
+typedef volatile unsigned short		vuint16_t;
+typedef volatile signed int		vint32_t;
+typedef volatile unsigned int		vuint32_t;
+typedef volatile signed long long	vint64_t;
+typedef volatile unsigned long long	vuint64_t;
+
+
+
+typedef signed char			sint8;
+typedef unsigned char			uint8;
+typedef signed short			sint16;
+typedef unsigned short			uint16;
+typedef signed int			sint32;
+typedef unsigned int			uint32;
+typedef signed long long		sint64;
+typedef unsigned long long		uint64;
+
+
+
+typedef volatile signed char		vint8_t;
+typedef volatile unsigned char	        vuint8_t;
+typedef volatile signed short		vint16_t;
+typedef volatile unsigned short		vuint16_t;
+typedef volatile signed int		vint32_t;
+typedef volatile unsigned int		vuint32_t;
+typedef volatile signed long long       vint64_t;
+typedef volatile unsigned long long     vuint64_64;
+#endif
